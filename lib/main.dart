4057d36b077a597e563655d49d13c72d0c3323d2
Jeremiah Opoku-Boateng <106244755+Tiptoplegend@users.noreply.github.com>
2024-12-09 20:54:33 +0000

    Changed title to Recipe

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Recipe Book app',
+      title: 'Recipe',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(
